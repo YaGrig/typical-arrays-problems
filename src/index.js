dbb6ec6580a4fa36eb8158e1fed65095b9a6fde1
YaGrig <yashagrigorev@gmail.com>
2021-02-27 09:48:24 +0300

    end

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+  if( !Array.isArray(array) || array.length == 0) {
+    return 0;
+  }
+  return Math.min.apply(Math,array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if( !Array.isArray(array) || array.length == 0){
+    return 0;
+  }
+  return Math.max.apply(Math,array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if( !Array.isArray(array) || array.length == 0){
+    return 0;
+  }
+  let result = 0;
+  for(i = 0; i<array.length; i++){
+      result = result + array[i]
+  }
+  return result/array.length;
 }
